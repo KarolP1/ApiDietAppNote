61615eae11c5a15bf00a2b5b6a2a6797fa27298a
KarolP1 <52258025+KarolP1@users.noreply.github.com>
2020-11-18 18:32:28 +0100

    update

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect("mongodb://localhost:27017/test", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
+mongoose.connect(
+	"mongodb+srv://KarolP:1236914789Mk@cluster0.mgvy8.mongodb.net/notes?retryWrites=true&w=majority",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
@@ -18,4 +21,4 @@ app.use(express.json());
 const notesRoute = require("./routes/notes.js");
 app.use("/api/notes", notesRoute);
 
-app.listen(PORT, () => console.log("Server Started"));
+app.listen(PORT, () => console.log(`Server Started on port: ${PORT}`));
